080f080447e4fa46d564645de522520b1b6f3460
hope <hopeobey06@gmail.com>
2026-06-05 16:25:25 +0100

    loops exercices done

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 console.log("Hello World");
-let userRandomWord = prompt("Enter a word :")
-console.log(userRandomWord)
-let wordTypedValidation = true
-if(wordTypedValidation) {
-    console.log("Great! you typed it right")
-} else {
-    console.log("oops, that wasn't correct")
+const givenWord = "morning"
+let userWord = prompt("Enter a word :" + givenWord)
+// console.log(userWord)
+// let wordTypedValidation = true
+// if(givenWord === userWord) {
+//     console.log("Great! you typed it right")
+// } else {
+//     console.log("oops, that wasn't correct")
+// }
+switch(userWord) {
+    case givenWord:
+        console.log("Great! you typed it right");
+        break;
+        case "fag":
+        console.log("not accepted");git 
+           break;
+        case "dork":
+        console.log("not accepted");
+           break;
+        case "idiot":
+        console.log("not accepted");
+           break;
+    default:
+        console.log("oops, that wasn't correct");
 }
